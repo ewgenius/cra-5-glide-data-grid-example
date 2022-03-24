0658de394afd4c7949de2c7a223d6bf08e585c0c
Evgenii Khramkov <ewgeniux@gmail.com>
2022-03-24 21:12:05 +0300

    setup @glideapps/glide-data-grid example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  DataEditor,
+  GridCell,
+  GridCellKind,
+  GridColumn,
+} from "@glideapps/glide-data-grid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface DummyItem {
+  name: string;
+  company: string;
+  phone: string;
+  email: string;
 }
+
+const data: DummyItem[] = [
+  {
+    name: "Deidre Morris",
+    company: "GONKLE",
+    email: "deidremorris@gonkle.com",
+    phone: "+1 (867) 507-3332",
+  },
+  {
+    name: "Sheryl Craig",
+    company: "EVENTAGE",
+    email: "sherylcraig@eventage.com",
+    phone: "+1 (869) 520-2227",
+  },
+  {
+    name: "Lidia Bowers",
+    company: "ANOCHA",
+    email: "lidiabowers@anocha.com",
+    phone: "+1 (808) 414-3826",
+  },
+];
+
+const App: React.VFC = () => {
+  const getContent = React.useCallback(
+    (cell: readonly [number, number]): GridCell => {
+      const [col, row] = cell;
+      const dataRow = data[row];
+      const indexes: (keyof DummyItem)[] = [
+        "name",
+        "company",
+        "email",
+        "phone",
+      ];
+      const d = dataRow[indexes[col]];
+      return {
+        kind: GridCellKind.Text,
+        allowOverlay: false,
+        displayData: d,
+        data: d,
+      };
+    },
+    []
+  );
+
+  const columns = React.useMemo<GridColumn[]>(() => {
+    return [
+      {
+        title: "Name",
+        id: "name",
+      },
+      {
+        title: "Company",
+        id: "company",
+      },
+      {
+        title: "Email",
+        id: "email",
+      },
+      {
+        title: "Phone",
+        id: "phone",
+      },
+    ];
+  }, []);
+
+  return (
+    <DataEditor
+      getCellContent={getContent}
+      columns={columns}
+      rows={data.length}
+    />
+  );
+};
 
 export default App;
